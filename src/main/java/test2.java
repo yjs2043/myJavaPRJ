529447347ec2389e37731344236aa6581bd6f0e8
yjs2043 <dydwnstnqls7@naver.com>
2023-03-15 10:01:42 +0900

    test

---
+++ src/main/java/test2.java
@@ -0,0 +1,7 @@
+public class test2 {
+    public static void main (String[] args){
+        System.out.println ("Memorizing");
+        System.out.println ("I'm not sure this is correct");
+        System.out.println ("Clear");
+    }
+}
