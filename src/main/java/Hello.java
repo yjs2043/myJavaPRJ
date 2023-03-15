eb9704ef4788956d31e8ec7aaa365e65990733be
yjs2043 <dydwnstnqls7@naver.com>
2023-03-15 09:24:14 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello!! My First Java Coding!!");
+        System.out.println("Can I ask what is coding?");
+
+    }
+}
