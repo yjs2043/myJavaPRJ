cae133b3a229a22cee31c0c32c8561ebb7afaf39
yjs2043 <dydwnstnqls7@naver.com>
2023-03-15 11:48:33 +0900

    Slide 10

---
+++ src/main/java/Ex03_02.java
@@ -0,0 +1,8 @@
+public class Ex03_02 {
+    public static void main(String[] args) {
+        System.out.printf("%d", 100, 200);
+        System.out.printf("\n");
+        System.out.printf("%d %d", 100);
+        System.out.printf("\n");
+    }
+}
