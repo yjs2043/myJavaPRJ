735c0e617764740a49551d6f4e12e02cccb4c89f
yjs2043 <dydwnstnqls7@naver.com>
2023-03-15 09:55:36 +0900

    test

---
+++ src/main/java/test1.java
@@ -0,0 +1,11 @@
+public class test1
+{
+    public static void main (String[] args )
+    {System.out.println("Testing1" );
+        System.out.println("Testing 2");
+        System.out.println("Testing 3");
+        System.out.println("Testing 4");
+        System.out.println("Finished");
+        System.out.println("Remember to type system.out.println before input texts");
+    }
+}
