04dfa3a77596044302c0d5c2d7d49720517a9c5d
yjs2043 <dydwnstnqls7@naver.com>
2023-03-15 11:05:03 +0900

    Slide 10

--- src/main/java/Ex03_01.java
+++ src/main/java/Ex03_01.java
@@ -1,8 +1,8 @@
 public class Ex03_01 {
     public static void main(String[] args) {
-        System.out.print("100+100");
-        System.out.print("\n");
-        System.out.print("%d 100 + 100)
-        System.out.print("\n");
+        System.out.printf("100+100");
+        System.out.printf("\n");
+        System.out.printf("%d", 100 + 100);
+        System.out.printf("\n");
     }
 }
