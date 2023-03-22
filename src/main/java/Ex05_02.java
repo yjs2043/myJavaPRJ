42819d31f9b91fa9cae810b7862ce2e3b43624b8
yjs2043 <dydwnstnqls7@naver.com>
2023-03-22 11:21:48 +0900

    Slide 05 P5

--- src/main/java/Ex05_02.java
+++ src/main/java/Ex05_02.java
@@ -3,9 +3,12 @@ public class Ex05_02 {
         int a = 200;
 
         if (a < 100)
+        {
             System.out.printf("100보다 작군요..\n");
             System.out.printf("거짓이므로 이 문장은 안보이겠죠?\n");
+        }
 
             System.out.printf("프로그램 끝! \n");
+
     }
 }
