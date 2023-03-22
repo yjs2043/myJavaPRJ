e05afe745e7f2a1d147a35e8575f5f8bae95a1f3
yjs2043 <dydwnstnqls7@naver.com>
2023-03-22 11:25:54 +0900

    Slide 05 P9

---
+++ src/main/java/Ex05_05.java
@@ -0,0 +1,15 @@
+public class Ex05_05 {
+    public static void main(String[] args) {
+        int a = 200;
+
+        if (a < 100) {
+            System.out.printf("100보다 작군요..\n");
+            System.out.printf("참이면 이 문장도 보이겠죠?\n");
+        } else {
+            System.out.printf("100보다 크군요..\n");
+            System.out.printf("거짓이면 이 문장도 보이겠죠\n");
+        }
+
+        System.out.printf("프로그램 끝! \n");
+    }
+}
