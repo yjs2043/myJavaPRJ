ec9beefbf832265954eae53bf62cdbc414c1f7da
yjs2043 <dydwnstnqls7@naver.com>
2023-03-29 10:50:38 +0900

    P 204

---
+++ src/main/java/Ex06_07.java
@@ -0,0 +1,13 @@
+public class Ex06_07 {
+    public static void main(String[] args) {
+        int hap = 0;
+        int i;
+
+        for (i = 1; i <= 10; i++) {
+            hap += 1;
+        }
+
+        System.out.printf(" 1에서 10까지의 합 : %d \n", hap);
+        }
+    }
+
