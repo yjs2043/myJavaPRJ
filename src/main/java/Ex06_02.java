3f2385fc0d210e37e8a9c6433131604abd7f4df3
yjs2043 <dydwnstnqls7@naver.com>
2023-03-29 10:28:56 +0900

    P 195

---
+++ src/main/java/Ex06_02.java
@@ -0,0 +1,10 @@
+public class Ex06_02 {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 5; i++)
+        {
+            System.out.printf("안녕하세요? 빙글빙글 for 문을 공부중입니다. &&\n");
+        }
+    }
+}
