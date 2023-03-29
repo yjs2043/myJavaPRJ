777c691ea87ba58e9fb5be028254eb48c9a02e01
yjs2043 <dydwnstnqls7@naver.com>
2023-03-29 10:16:44 +0900

    P 181

---
+++ src/main/java/Ex05_09.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ex05_09 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.printf("1~4 중에 선택하세요 :");
+        a = s.nextInt();
+
+        switch (a) {
+            case 1:
+                System.out.printf("1을 선택했다\n");
+                break;
+            case 2:
+                System.out.printf("2을 선택했다\n");
+                break;
+            case 3:
+                System.out.printf("3을 선택했다\n");
+                break;
+            case 4:
+                System.out.printf("4를 선택했다\n");
+                break;
+            default:
+                System.out.printf("이상한걸 선택했다\n");
+        }
+    }
+}
